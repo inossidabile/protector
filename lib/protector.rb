60614c4b067e610f9720bb9a7769d2cffdec1179
Boris Staal <boris@staal.io>
2013-05-22 23:27:41 +0700

    ActiveRecord adapter automatic activation

--- lib/protector.rb
+++ lib/protector.rb
@@ -6,6 +6,8 @@ require "protector/adapters/active_record"
 
 I18n.load_path << Dir[File.join File.expand_path(File.dirname(__FILE__)), '..', 'locales', '*.yml']
 
+Protector::Adapters::ActiveRecord.activate! if defined?(ActiveRecord)
+
 module Protector
   # Your code goes here...
 end
