d5932432240140557aa3bfc68933f31553971628
Boris Staal <boris@staal.io>
2013-06-04 18:00:43 +0700

    I18n inclusion fixed

--- lib/protector.rb
+++ lib/protector.rb
@@ -6,7 +6,7 @@ require "protector/dsl"
 require "protector/adapters/active_record"
 require "protector/adapters/sequel"
 
-I18n.load_path << Dir[File.join File.expand_path(File.dirname(__FILE__)), '..', 'locales', '*.yml']
+I18n.load_path += Dir[File.expand_path File.join('..', 'locales', '*.yml'), File.dirname(__FILE__)]
 
 Protector::Adapters::ActiveRecord.activate! if defined?(ActiveRecord)
 Protector::Adapters::Sequel.activate! if defined?(Sequel)
